3a8dbcaa0ac847dbe7609268a0d34642dad3ca54
Idan <idanelchalal@gmail.com>
2023-03-26 16:26:56 +0300

    CLIENT WORK

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,15 +4,35 @@ import io from 'socket.io-client'
 const socket = io('http://localhost:3001')
 
 function App() {
-    const [flights, setFlights] = useState({})
+    const [landed, setLanded] = useState({})
+    const [departure, setDeparture] = useState({})
 
     useEffect(() => {
-        socket.on('message', (data) => {
-            setFlights(data)
-        })
+        socket.on('message', (context) => {})
     }, [])
 
-    return <div></div>
+    return (
+        <>
+            <div className="container">
+                <header className="align-items-center justify-content-center">
+                    TERMINALY
+                </header>
+                <nav className="align-items-center justify-content-center">
+                    Flights Panel
+                </nav>
+                <article>
+                    <section>
+                        <h1 className="text-align-center">Landed:</h1>
+                        <ul></ul>
+                    </section>
+                    <section>
+                        <h1 className="text-align-center">Departure:</h1>
+                        <ul></ul>
+                    </section>
+                </article>
+            </div>
+        </>
+    )
 }
 
 export default App
